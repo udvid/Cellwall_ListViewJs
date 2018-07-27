2930487549e4bc31da6cb602a171e018001c1713
Udvid <34854446+udvid@users.noreply.github.com>
2018-07-27 09:36:04 +0530

    Create ListView.js

---
+++ ListView.js
@@ -0,0 +1,58 @@
+/**
+ * ----------------------------------------------
+ * ListView class
+ * ----------------------------------------------
+ * @param dataSource
+ * @param template
+ * @constructor
+ */
+Cw.ListView = function (dataSource, template) {
+    this.DataSource = dataSource;
+    this.Template = template;
+};
+
+Cw.ListView.prototype = {
+    /**
+     * ------------------------------------------
+     * Replace template tags with variable data
+     * ------------------------------------------
+     * @param content
+     * @param data
+     * @returns {*}
+     * @constructor
+     */
+    Replace: function (content, data) {
+        var newContent = content;
+        Object.keys(data).forEach(function(key) {
+            newContent = newContent.replace("[@" + key + "]", data[key]);
+        });
+        return newContent;
+    },
+
+    /**
+     * --------------------------------------------
+     * Create a simple list
+     * --------------------------------------------
+     * @returns {*}
+     * @constructor
+     */
+    Create: function () {
+        return this.Replace(this.Template, this.DataSource);
+    },
+
+    /**
+     * ---------------------------------------------
+     * Create a list for database resultset
+     * ---------------------------------------------
+     * @returns {string}
+     * @constructor
+     */
+    CreateBlock: function () {
+        var output = "";
+        for (var i = 0; i < this.DataSource.length; i++)
+        {
+            output += this.Replace(this.Template, this.DataSource[i]);
+        }
+        return output;
+    }
+};
